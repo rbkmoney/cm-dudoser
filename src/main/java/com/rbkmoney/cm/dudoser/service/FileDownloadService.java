7dc180b1010e9dfd7651a876dc8922489092b192
vitaxa <vitaxa93gamebox@gmail.com>
2020-04-17 18:19:29 +0300

    BJ-851: fix storage url (#8)

--- src/main/java/com/rbkmoney/cm/dudoser/service/FileDownloadService.java
+++ src/main/java/com/rbkmoney/cm/dudoser/service/FileDownloadService.java
@@ -3,12 +3,14 @@ package com.rbkmoney.cm.dudoser.service;
 import com.rbkmoney.cm.dudoser.service.model.FileInfo;
 import com.rbkmoney.file.storage.FileData;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Collections;
 
 @Slf4j
@@ -20,6 +22,7 @@ public class FileDownloadService {
 
     private final FileStorageService fileStorageService;
 
+    @SneakyThrows
     public FileInfo requestFile(String url, String fileId) {
         try {
             FileData fileData = fileStorageService.getFileData(fileId);
@@ -27,7 +30,7 @@ public class FileDownloadService {
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
             HttpEntity<String> entity = new HttpEntity<>(headers);
-            ResponseEntity<byte[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, byte[].class);
+            ResponseEntity<byte[]> response = restTemplate.exchange(new URI(url), HttpMethod.GET, entity, byte[].class);
 
             return new FileInfo(fileName, response.getBody());
         } catch (RestClientException e) {
